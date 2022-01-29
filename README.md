1304f6c7f81c9e9a642b7a57629dea1860ffbfe4
John <jrw@mit.edu>
2022-01-29 11:18:55 -0500

    reflexSpeed

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# pacmandata
+# pacmandata - author jrw@mit.edu
+
+## Server serves up Web Page with Pacman
+
+## Click on Pacman and fetch pushes data to Server
+
+## Server displays it has got the data
+
+## Server could store data in DB or push to a Broker
--- server.js
+++ server.js
@@ -1,25 +1,30 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-var app = express();
+let express = require('express');
+let bodyParser = require('body-parser');
+let path = require('path');
+let app = express();
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var data = [];
-var root = path.resolve(__dirname) + '/public/';
+let data = [99999];
+
+let root = path.resolve(__dirname) + '/public/';
 app.get('/', (req, res) => {
     res.sendFile('block.html', { 'root': root });
 });
 app.get('/set', function(req, res) {
-    console.log(JSON.stringify(req.query));
-    data.push({ x: req.query.x });
-    res.send({
-        x: req.query.x
-    });
+    
+    x = parseInt(req.query.x)
+    data.push(x);
+    fastest = Math.min(...data);
+    console.log("fastest:" + fastest);
+    console.log("data:" + data);
 });
 app.get('/get', function(req, res) {
     res.send(JSON.stringify(data));
 });
+app.get('/fastest',function(req,res){
+    res.json(fastest)
+})
 app.get('/pac', function(req, res) {
     res.sendFile('/PacMan' + req.query.id + '.png', { 'root': root });
 });
